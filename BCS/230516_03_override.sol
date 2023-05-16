a1054ce547df098acabef0ad6e67855c2b1e108a
Errly223093 <lmjgmm@gmail.com>
2023-05-16 17:02:30 +0800

    9th commit

---
+++ BCS/230516_03_override.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.18;
+
+contract A {
+    uint public a;
+    uint public b; 
+    constructor(uint _a, uint _b) {
+        a = _a;
+        b = _b;
+    }
+}
+
+contract B {
+
+}
+
+contract C {
+
+}
